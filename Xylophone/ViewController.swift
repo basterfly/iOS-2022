0829bbef60bbf19c14566c1412ef01c49f0d2cd9
Yehor <basterfly@gmail.com>
2023-02-17 17:29:21 +0200

    Finished: with corrections and challenge task
    added:
    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
    //This code brings sender's opacity back to fully opaque.
                sender.alpha = 1.0
                print("End")
            }

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,12 +18,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print(sender.currentTitle)
-        playSound(pressedButtonTitle: sender.currentTitle!)
+//        print(sender.currentTitle)
+        //Reduces the opacity of the Button to half (the selected Button)
+        print("Start")
+        sender.alpha = 0.5
+        //this line of code will help you to delay the opacity to the selected seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //This code brings sender's opacity back to fully opaque.
+            sender.alpha = 1.0
+            print("End")
+        }
+        playSound(soundName: sender.currentTitle!)
+        
     }
     
-    func playSound(pressedButtonTitle: String) {
-        let url = Bundle.main.url(forResource: pressedButtonTitle, withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
     }
