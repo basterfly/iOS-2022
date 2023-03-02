f7e6425503e9621577e18a19f1bfa22b48bbbbb7
Yehor <basterfly@gmail.com>
2023-03-02 23:05:54 +0200

    Timer: countdown seconds to end
    Uses: if statement for checking nil value;
    and Timer.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,23 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTimes = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
+    var counter = 0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
-        switch hardness {
-        case "Soft" :
-            print(softTime)
-        case "Medium" :
-            print(mediumTime)
-        case "Hard" :
-            print(hardTime)
-        default :
-            print("Error")
+        if sender.currentTitle != nil {
+            let hardness = sender.currentTitle!
+            counter = eggTimes[hardness]! * 60
+            print(eggTimes[hardness]!) // just for test
+            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         }
     }
-    
-
+ 
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds left")
+            counter -= 1
+        }
+    }
 }
