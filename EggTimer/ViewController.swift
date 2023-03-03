d8d8a8084b4faffd59e036d68e08da4ff8fe3c82
Yehor <basterfly@gmail.com>
2023-03-03 20:27:33 +0200

    Timer Countdown + progressView
    My first variant with progressView, titleImage shows seconds to end

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,24 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     let eggTimes = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
     var counter = 0
+    var timer = Timer()
+    var progressBarMax: Float = 0.0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
         if sender.currentTitle != nil {
             let hardness = sender.currentTitle!
             counter = eggTimes[hardness]! * 60
-            print(eggTimes[hardness]!) // just for test
-            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+            progressBarMax = Float(counter)
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         }
     }
  
     @objc func updateCounter() {
-        //example functionality
         if counter > 0 {
-            print("\(counter) seconds left")
+//            print("\(counter) seconds left")
+            titleLabel.text = "\(counter) seconds left"
+            progressBar.progress = Float(counter) / progressBarMax
             counter -= 1
+        } else {
+            progressBar.progress = 0
+            titleLabel.text = "DONE!"
         }
     }
 }
