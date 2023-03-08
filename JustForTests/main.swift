11d803f6b123d0902854c8e815a8be372af70f47
Yehor <basterfly@gmail.com>
2023-03-08 19:04:08 +0200

    Test 4: Leap year
    - if - else
    - random

--- JustForTests/main.swift
+++ JustForTests/main.swift
@@ -7,22 +7,34 @@
 
 import Foundation
 
-// Challenge 82 switch Angela Yu
+//Leap year
+/*
+The 3 conditions for any given year to be a leap year are:
+The year is exactly divisible by four (with no reminder) then it is a leap year, unless:
+If the year is also divisible by 100 (years ending in two zeros), then it is not a leap year, except if
+It is also divisible by 400 (in this case it will be a leap year).
+Hint: You can check Wikipedia for the conditions expressed in your native language.
+For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+If in doubt you can check the year here:
+http://time-and-calendar.com/leap-years
+Complete the isLeap() function to print "YES" if the input is a leap year and "NO" if the input year was not.
+*/
 
-func loveCalculator() {
-    var loveScore = Int.random(in: 0 ... 100)
-    print(loveScore)
-    switch loveScore {
-    case (80 ... 100) :
-        print("You love each other like Kanye love Kanye")
-    case (40 ... 79) :
-        print("You go toghether like coke and Mentos")
-    case (0 ... 39) :
-        print("You`ll be forever alone")
-    default :
-        print("Error")
+func isLeap(year: Int) {
+    if year % 4 == 0 {
+        if (year % 100) == 0 {
+            if year % 400 == 0 {
+                print(year, " is a LEAP year!")
+            } else {
+                print("\(year) is NOT a leap year!")
+            }
+        } else {
+            print("\(year) is a LEAP year!")
+        }
+    } else {
+        print("\(year) is NOT a leap year!")
     }
 }
 
-loveCalculator()
-
+isLeap(year: Int.random(in: 1000 ... 3000) )
+ 
