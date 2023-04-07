97947dbf88dbcf141d83bcee6dfa8929166d2ad2
Yehor <basterfly@gmail.com>
2023-04-07 20:33:02 +0300

    First step of developing logic with MVC

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,10 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var number = 0
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.titleLabel == choice1Button.titleLabel {
+            number = 1
+            buttonPressed()
+        } else {
+            number = 2
+            buttonPressed()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        buttonPressed()
+    }
+    
+    func buttonPressed() {  //updateUI()
+        let story = getAStory(number)
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
     }
 
 
